8c04570ea488db3a008af391df9bb24d893db1ca
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2024-11-11 21:07:01 +0100

    poprawiony alert po akcjach na poście

--- resources/views/szablon/szablon.blade.php
+++ resources/views/szablon/szablon.blade.php
@@ -76,12 +76,12 @@
                                 </div>
 
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900">@yield('podtytul')</h2>
-                                @if(session()->has('message'))
-                                <div class="alert alert-@if(session()->has('class')){{session()->get('class')}}@else{{"success"}}@endif alert-dismissable fade show">
-                                    <strong>{{session()->get('message')}}</strong>
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                </div>
-                                @endif
+                                @if (session()->has('message'))
+                <div class="alert alert-@if(session()->has('class')){{session()->get('class')}}@else{{ "success" }}@endif alert-dismissible fade show" role="alert">
+                    <strong>{{ session()->get('message') }}</strong>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div> 
+                @endif
                                 <p class="mt-4 text-gray-500 text-sm leading-relaxed w-100">
                                     @yield('tresc', "nie zawiera żadnej treści")
                                 </p>
